7ae254dae792eb117b7e9d396a36675c63afabc3
Felipe Daiello <fdaiello@misterpostman.com.br>
2022-02-12 12:07:01 -0300

    NumberOfWaysToMakeChange

--- ConsoleApp1/Dp.cs
+++ ConsoleApp1/Dp.cs
@@ -7,6 +7,57 @@ namespace AlgoTests
     public class Dp
     {
 		/*
+		 * https://www.algoexpert.io/questions/Number%20Of%20Ways%20To%20Make%20Change
+		 * 
+		 *  Input:
+		 *         n- the amout to make change
+		 *         denoms - collection of coins denomination ( infinite set of each denomination )
+		 *         
+		 *  Output
+		 *         the number of different ( unordered ) possibilites to make the change
+		 */
+		public static int NumberOfWaysToMakeChange(int n, int[] denoms)
+		{
+
+			// Memo array f - starting from zero - f[0] inits with 1 ( one possibility to make change for 0 with zero coins )
+			int[] f = new int[n + 1];
+			f[0] = 1;
+
+			// Test each coin
+			foreach ( int coin in denoms)
+            {
+				// Test all subamounts 
+				for ( int a = 1; a<=n; a++)
+                {
+					// Check if coin is lower or equal denomination
+					if ( coin <= a )
+                    {
+						// Add this possibility to memo array - one possibility ( with this coin ) plus all possibilities already computed for this amount subtractin this coin
+						f[a] += f[a - coin];
+					}
+                }
+            }
+
+			// Return f for largest amount - n
+			return f[n];
+
+		}
+		public static void TestNumberOfWaysToMakeChange()
+        {
+			int[] denomn = new int[] { 1, 5 };
+			int n = 6;
+
+			Console.WriteLine(NumberOfWaysToMakeChange(n, denomn));
+			Console.WriteLine("Expected: 2");
+
+
+			denomn = new int[] { 1, 3, 4 };
+			n = 6;
+			Console.WriteLine(NumberOfWaysToMakeChange(n, denomn));
+			Console.WriteLine("Expected: 4");
+
+		}
+		/*
 		 *  https://www.algoexpert.io/questions/Max%20Subset%20Sum%20No%20Adjacent
 		 */
 		public static int MaxSubsetSumNoAdjacent(int[] array)
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,7 +9,7 @@ namespace AlgoTests
     {
         static void Main(string[] args)
         {
-            Dp.TestMaxSubsetSumNoAdjacent();
+            Dp.TestNumberOfWaysToMakeChange();
         }
     }
 }
