9af6bd07bf0b01bf3dd991c2de794de56bc7f7ac
Felipe Daiello <fdaiello@misterpostman.com.br>
2022-02-25 17:11:05 -0300

    FindNodeDistanceK

--- ConsoleApp1/BTSolutions.cs
+++ ConsoleApp1/BTSolutions.cs
@@ -1,9 +1,147 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace AlgoTests
 {
     public class BTSolutions
     {
+		#region FindNodeDistanceK
+		/*
+		 * https://www.algoexpert.io/questions/Find%20Nodes%20Distance%20K
+		 */
+
+		public static List<int> FindNodesDistanceK(BinaryTree tree, int target, int k)
+		{
+			// List of returning nodes
+			List<int> list = new List<int>();
+
+			// Create a map with parents node
+			Dictionary<int, BinaryTree> parentsMap = new Dictionary<int, BinaryTree>();
+			MapParents(tree, null, ref parentsMap);
+
+			// Find target Node
+			BinaryTree targetNode = FindNode(tree, target);
+
+			// Traverse Tree as a Graph, from target Node
+			HashSet<int> visited = new HashSet<int>();
+			Queue<Tuple<BinaryTree, int>> queue = new Queue<Tuple<BinaryTree, int>>();
+			queue.Enqueue(new Tuple<BinaryTree, int>(targetNode, 0));
+
+			while ( queue.Count > 0)
+            {
+				var node = queue.Dequeue();
+				
+				
+				if (visited.Contains(node.Item1.value))
+					continue;
+				visited.Add(node.Item1.value);
+
+				if (node.Item2 == k)
+					list.Add(node.Item1.value);
+
+				if ( node.Item1.left != null)
+					queue.Enqueue(new Tuple<BinaryTree, int>(node.Item1.left, node.Item2 + 1));
+				if (node.Item1.right != null)
+					queue.Enqueue(new Tuple<BinaryTree, int>(node.Item1.right, node.Item2 + 1));
+				if (parentsMap.ContainsKey(node.Item1.value))
+					queue.Enqueue(new Tuple<BinaryTree, int>(parentsMap[node.Item1.value], node.Item2 + 1));
+			}
+
+			return list;
+		}
+		public static void MapParents(BinaryTree node, BinaryTree parent, ref Dictionary<int, BinaryTree> parentsMap)
+        {
+			if (node == null)
+				return;
+
+			if (parent != null)
+				parentsMap.Add(node.value, parent);
+
+			MapParents(node.left, node, ref parentsMap);
+			MapParents(node.right, node, ref parentsMap);
+
+        }
+		public static BinaryTree FindNode(BinaryTree node, int value)
+        {
+			if (node == null)
+				return null;
+
+			else if (node.value == value)
+				return node;
+
+			BinaryTree node1 = FindNode(node.left, value);
+			if (node1 != null)
+				return node1;
+
+			return FindNode(node.right, value);
+        }
+		public static void TestFindNodesDistanceK()
+        {
+			BinaryTree tree = new BinaryTree(1)
+			{
+				left = new BinaryTree(2)
+				{
+					left = new BinaryTree(4)
+					{
+						left = new BinaryTree(41)
+					},
+					right = new BinaryTree(5)
+				},
+				right = new BinaryTree(3)
+				{
+					right = new BinaryTree(6)
+                    {
+						left = new BinaryTree(7),
+						right = new  BinaryTree(8)
+                    }
+				}
+			};
+
+			int target = 3;
+			int k = 2;
+			Console.WriteLine(String.Join(",",FindNodesDistanceK(tree,target,k)));
+			Console.WriteLine("Expected: 2, 7, 8");
+
+        }
+
+		// This trial didn't work
+		public static List<int> FindNodesDistanceK_bug(BinaryTree tree, int target, int k)
+		{
+			List<int> list = new List<int>();
+			int? currentDistance = null;
+			FindNodes_bug(tree, target, k, ref currentDistance, ref list);
+
+			return list;
+		}
+		public static void FindNodes_bug(BinaryTree tree, int target, int k, ref int? distance, ref List<int> list)
+		{
+			int? oldDistance = distance;
+
+			if (tree == null)
+				return;
+
+			if (tree.value == target)
+				distance = 0;
+			else if (distance != null)
+				distance = Math.Abs(distance ?? 0) + 1;
+
+			if (distance == k && !list.Contains(tree.value))
+				list.Add(tree.value);
+
+			FindNodes_bug(tree.left, target, k, ref distance, ref list);
+			FindNodes_bug(tree.right, target, k, ref distance, ref list);
+
+			if (oldDistance == null && distance != null)
+			{
+				FindNodes_bug(tree.left, target, k, ref distance, ref list);
+				FindNodes_bug(tree.right, target, k, ref distance, ref list);
+			}
+
+			distance--;
+		}
+
+
+		#endregion
 		#region HeightBalancedTree
 		/*
 		 * https://www.algoexpert.io/questions/Height%20Balanced%20Binary%20Tree
