bcbd946d97cf70358b98b0bc94e096714bfae637
Felipe Daiello <fdaiello@misterpostman.com.br>
2022-04-05 09:55:41 -0300

    LongestSubstringWithoutDuplication

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,7 +10,7 @@ namespace AlgoTests
         static void Main(string[] args) 
         {
 
-            ArraySolutions.TestTwoNumberSum();
+            StringSolutions.TestLongestSubstring();
 
         }
     }
